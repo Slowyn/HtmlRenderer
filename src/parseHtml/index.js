782f95a07a463cfa92b1510802b7e267fa107685
Slowyn <y231966mdk@gmail.com>
2017-09-19 22:24:11 +0700

    Implement html parsing

---
+++ src/parseHtml/index.js
@@ -0,0 +1,20 @@
+import htmlparser from 'htmlparser2-without-node-native';
+
+export default function parseHtml(rawHtml) {
+  const options = {
+    recognizeSelfClosing: true,
+    lowerCaseAttributeNames: true,
+    lowerCaseTags: true,
+    decodeEntities: true,
+  };
+  let parsedHtml = [];
+  const parser = new htmlparser.Parser(
+    new htmlparser.DomHandler((_err, dom) => {
+      parsedHtml = dom;
+    }),
+    options,
+  );
+  parser.write(rawHtml);
+  parser.end();
+  return parsedHtml;
+}
